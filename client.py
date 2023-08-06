640810267488a7f2acf6b49de8b604b64578b004
MasterCoookie <jas.kocurek2@wp.pl>
2023-08-06 20:52:50 +0200

    threads wip

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import time
 import socket
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QLineEdit, QWidget
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, QObject, pyqtSignal, QThread
 
-class UdpClient:
+class UdpClient(QObject):
+    finished = pyqtSignal(str)
+
     def __init__(self):
+        super().__init__()
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_socket.settimeout(2.0)
 
@@ -15,14 +18,16 @@ class UdpClient:
         message = _message.to_bytes(2, 'big')
 
         self.client_socket.sendto(message, addr)
+        result = ''
         try:
             data, server = self.client_socket.recvfrom(1024)
             data = int.from_bytes(data, 'big')
             end = time.time()
             elapsed = end - start
-            return f'{data} in {elapsed}s'
+            result = f'{data} in {elapsed}s'
         except socket.timeout:
-            return 'REQUEST TIMED OUT'
+            result = 'REQUEST TIMED OUT'
+        self.finished.emit(result)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -59,13 +64,28 @@ class MainWindow(QMainWindow):
         container = QWidget()
         container.setLayout(self.layout)
         self.setCentralWidget(container)
+
+    def on_finished(self, result):
+        self.button.setEnabled(True)
+        self.result_label.setText(result)
     
     def on_click(self):
+        self.button.setEnabled(False)
+        
         value = int(self.value_input.text())
         address = self.address_input.text()
-        result = self.client.send_message(value, address)
-        self.result_label.setText(f'Result: {result}')
 
+        self.thred = QThread()
+        self.client.moveToThread(self.thred)
+        self.thred.started.connect(self.client.send_message, value, address)
+        self.thred.finished.connect(self.thred.quit)
+        self.client.finished.connect(self.client.deleteLater)
+        self.thred.finished.connect(self.thred.deleteLater)
+        self.thred.start()
+
+        self.thred.finished.connect(self.on_finished)
+
+    
 
 if __name__ == '__main__':
     app = QApplication([])
