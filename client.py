6e37254c71470c000626b049e1853d904dc91578
MasterCoookie <jas.kocurek2@wp.pl>
2023-08-06 19:58:09 +0200

    moves udp to class

--- client.py
+++ client.py
@@ -3,6 +3,26 @@ import socket
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtCore import QSize
 
+class UdpClient:
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client_socket.settimeout(1.0)
+        self.message = 2
+        self.addr = ("127.0.0.1", 12000)
+
+    def send_message(self):
+        start = time.time()
+
+        self.client_socket.sendto(self.message.to_bytes(2, 'big'), self.addr)
+        try:
+            data, server = self.client_socket.recvfrom(1024)
+            data = int.from_bytes(data, 'big')
+            end = time.time()
+            elapsed = end - start
+            print(f'Recieved response:{data} in {elapsed}s')
+        except socket.timeout:
+            print('REQUEST TIMED OUT')
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -16,25 +36,9 @@ class MainWindow(QMainWindow):
         self.button.move(190, 20)
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.settimeout(1.0)
-message = 2
-addr = ("127.0.0.1", 12000)
-
 app = QApplication([])
 
 window = MainWindow()
 window.show()
 
 app.exec_()
-
-start = time.time()
-client_socket.sendto(message.to_bytes(2, 'big'), addr)
-try:
-    data, server = client_socket.recvfrom(1024)
-    data = int.from_bytes(data, 'big')
-    end = time.time()
-    elapsed = end - start
-    print(f'Recieved response:{data} in {elapsed}s')
-except socket.timeout:
-    print('REQUEST TIMED OUT')
